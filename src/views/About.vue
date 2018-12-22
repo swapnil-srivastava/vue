08f2ca50d56ce579f34316649937594c8265a045
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2018-12-23 04:43:57 +0530

    adding changes to the about page to show the change getting reflected

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Vue app with Typescript in firebase and ci/cd done by circle ci </h1>
+    <h2>Build by Swapnil Srivastava </h2>
   </div>
 </template>
